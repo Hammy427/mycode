e6c7bece3a332b71d4554645c94e08512680b786
Hammy427 <Phillip.M.Vasconcellos@Wgmail.com>
2024-08-02 14:36:05 +0000

    Learning about regular expression

---
+++ classinfo01.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+#Create Classinfo
+
+classinfo = {
+    "all": [
+        {
+            "name": "Aaron",
+            "skill level": "wondrous",
+            "spirit animal": "Alpaca",
+            "super power": "Structure Weakening",
+        },
+        {
+            "name": "Alexandra",
+            "skill level": "admirable",
+            "spirit animal": "Shark",
+            "super power": "Super Strength",
+        },
+        {
+            "name": "Alice",
+            "skill level": "amazing",
+            "spirit animal": "Goat",
+            "super power": "Weather Control",
+        },
+        {
+            "name": "Angela",
+            "skill level": "astonishing",
+            "spirit animal": "Banana",
+            "super power": "Flight",
+        },
+        {
+            "name": "Ayrat",
+            "skill level": "awesome",
+            "spirit animal": "Horse",
+            "super power": "X-ray Vision",
+        },
+        {
+            "name": "Blake",
+            "skill level": "brilliant",
+            "spirit animal": "Eagle",
+            "super power": "Shape of: A Giant Shark",
+        },
+        {
+            "name": "Brian",
+            "skill level": "cool",
+            "spirit animal": "Rabbit",
+            "super power": "Invisibility",
+        },
+        {
+            "name": "Carl",
+            "skill level": "enjoyable",
+            "spirit animal": "Water buffalo",
+            "super power": "Transformation",
+        },
+        {
+            "name": "Chris",
+            "skill level": "excellent",
+            "spirit animal": "Chicken",
+            "super power": "Pyrokinesis",
+        },
+        {
+            "name": "Cliff",
+            "skill level": "fabulous",
+            "spirit animal": "Duck",
+            "super power": "Invulnerability",
+        },
+        {
+            "name": "Daniel",
+            "skill level": "fantastic",
+            "spirit animal": "Goose",
+            "super power": "Explosive Shouts",
+        },
+        {
+            "name": "Etien",
+            "skill level": "great",
+            "spirit animal": "Pigeon",
+            "super power": "Matter Ingestion",
+        },
+        {
+            "name": "Felicia",
+            "skill level": "incredible",
+            "spirit animal": "Turkey",
+            "super power": "Zoolingualism",
+        },
+        {
+            "name": "Gabriel",
+            "skill level": "magnificent",
+            "spirit animal": "Aardvark",
+            "super power": "Height Manipulation",
+        },
+        {
+            "name": "Jacob",
+            "skill level": "marvelous",
+            "spirit animal": "Aardwolf",
+            "super power": "Hydrokinesis",
+        },
+        {
+            "name": "Joe",
+            "skill level": "outstanding",
+            "spirit animal": "Elephant",
+            "super power": "Needle Projection",
+        },
+        {
+            "name": "Paije",
+            "skill level": "phenomenal",
+            "spirit animal": "Leopard",
+            "super power": "Stretchy",
+        },
+        {
+            "name": "Phillip",
+            "skill level": "pleasant",
+            "spirit animal": "Albatross",
+            "super power": "Steel Skin",
+        },
+        {
+            "name": "Magnus",
+            "skill level": "pleasing",
+            "spirit animal": "Alligator",
+            "super power": "Teleportation",
+        },
+        {
+            "name": "Samuel",
+            "skill level": "remarkable",
+            "spirit animal": "Lynx",
+            "super power": "Polyglot",
+        },
+        {
+            "name": "Zachary",
+            "skill level": "super",
+            "spirit animal": "Fox",
+            "super power": "Eat Anything",
+        },
+    ]
+}
+
+
+
+
+
+
+ classinfo = {"name":"Alice", "Spirit Animal":"Goat" }
+ print(classinfo)
+
+~                                                           
